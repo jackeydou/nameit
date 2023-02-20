7372ec9666d5edf4e333fcab2650c6920790b5ca
XHeapStack <jackey.dou@gmail.com>
2023-02-20 21:20:38 +0800

    feat: use vercel edge function

--- src/api/web.ts
+++ src/api/web.ts
@@ -1,30 +1,18 @@
 import axios from "axios";
-import { OPEN_AI_HOST, OPEN_AI_KEY, OpenAIResponse, getVariblePrompt, VariableNamingConventions } from './openai';
+import { OpenAIResponse, getVariblePrompt, VariableNamingConventions } from './openai';
 import { removeNewlines } from "../utils";
-
 
 export async function openAIVaribleNamingApi(
   prompt: string,
   convention: VariableNamingConventions
 ) {
-  if (!OPEN_AI_KEY) {
-    throw new Error("Missing OpenAI OPEN_AI_KEY");
-  }
-  const url = `${OPEN_AI_HOST}/v1/completions`;
+
+  const url = '/api/edge';
   const response = await axios.post<OpenAIResponse>(url, {
-    model: "text-davinci-003",
     prompt: getVariblePrompt(prompt, convention),
-    max_tokens: 100,
-    temperature: 0,
-    top_p: 1,
-    n: 1,
-    stream: false,
-    logprobs: null,
-    stop: "$$",
   }, {
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${OPEN_AI_KEY}`,
     },
   });
   const { data } = response;
