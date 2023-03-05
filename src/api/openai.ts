dcab18c8ff40458a6c29eb42804fd2e1b15600c2
XHeapStack <jackey.dou@gmail.com>
2023-03-05 23:12:44 +0800

    feat: change openai api domain proxy

--- src/api/openai.ts
+++ src/api/openai.ts
@@ -1,4 +1,4 @@
-export const OPEN_AI_HOST = "https://api.openai.com";
+export const OPEN_AI_HOST = "https://openai.proxyapiiii.xyz"; //"https://api.openai.com";
 
 export const OPEN_AI_KEY = import.meta.env.VITE_OPEN_AI_KEY;
 
